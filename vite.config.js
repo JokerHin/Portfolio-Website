9cebd74b63abb7be18903a8bfa0cb1997c49bd96
JokerHin <chokarhin@gmail.com>
2024-12-30 18:21:45 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: [
-      'swiper/react',
-      'swiper/css', // Add swiper/css here for pre-bundling
-      "tsparticles",
-      "@tsparticles/react",
-      "lucide-react",
-      "lottie-react",
-      "sweetalert2",
-      'swiper/css/pagination',
-    ],
-  },
-  build: {
-    rollupOptions: {
-      external: [
-        "lucide-react",
-        "tsparticles",
-        "@tsparticles/react",
-        "swiper/react",
-        'swiper/css',
-        "lottie-react",
-        "sweetalert2",
-        'swiper/css/pagination',
-      ],
-    },
-  },
-});
+})
